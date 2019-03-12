e0b6050e91e9f21efccbf1f166f4b6f37c0372fe
Guitou <guillaume.lagay@etu.univ-amu.fr>
2019-03-12 10:15:49 +0100

    Ajout d'un message si pas de paramètre.

--- src/fr/matthieu42/lightshotbot/command/SpamImage.java
+++ src/fr/matthieu42/lightshotbot/command/SpamImage.java
@@ -8,21 +8,25 @@ import java.math.BigInteger;
 public class SpamImage extends Command {
 
     public SpamImage() {
-        super("SpamImage","Spamme des images random de lightshot");
+        super("SpamImage","Spam des images random de lightshot");
     }
 
     @Override
     public void execute(MessageReceivedEvent event) {
+        if (event.getMessage().getContentRaw().equals("!SpamImage")) {
+            TextChannel textChannel = event.getTextChannel();
+            textChannel.sendMessage("Indique les 6 lettres de l'URL à laquelle tu veux commencer fdp").complete();
+        }
+
+        String valueDepart = event.getMessage().getContentRaw().replaceFirst("!SpamImage ","");
 
         TextChannel textChannel = event.getTextChannel();
-        BigInteger d = BigInteger.valueOf(621939073);
+        BigInteger d = BigInteger.valueOf(Integer.parseInt(valueDepart,36));
         Boolean run = true;
         while(run){
-            if(d.compareTo(BigInteger.valueOf(Long.parseLong("2176782335"))) > 0 )
-                run = false;
             d = d.add(BigInteger.valueOf(1));
             String message = "https://prnt.sc/" +  Integer.toString(Integer.parseInt(d.toString()),36);
-            textChannel.sendMessage(message).complete() ;
+            textChannel.sendMessage(message).complete();
             System.out.println(message);
             try {
                 Thread.sleep(5000);
